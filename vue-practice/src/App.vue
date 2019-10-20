457d5953dd0a0ae83db5429bc284cee5208de4ce
Durdona A <holisihlos@gmail.com>
2019-10-20 14:59:55 -0400

    adding header

---
+++ vue-practice/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- has to have only one element inside, adding an extra div will cause error -->
+  <div id="app">
+    <Todos />
+    <!-- Todos component -->
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos";
+export default {
+  name: "app",
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todo: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo False",
+          completed: false
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
