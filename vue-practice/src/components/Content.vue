1b102448b6b04dbb5f182e60dca92285f72c4d8d
Durdona A <holisihlos@gmail.com>
2019-10-21 12:05:26 -0400

    animation

--- vue-practice/src/components/Content.vue
+++ vue-practice/src/components/Content.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="content">
-    <img
-      class="eiffel"
-      :src="eiffel_tower"
-      :alt="Eiffel_Tower"
-      v-bind:style="{width: my_width + 'px'}"
-    />
+    <transition name="fade" >
+      <img
+        @mouseover="hover=false"
+        @mouseleave="hover = true"
+        v-show="hover" v-cloak
+        class="eiffel"
+        :src="eiffel_tower"
+        :alt="Eiffel_Tower"
+        v-bind:style="{width: my_width + 'px'}"
+      />
+    </transition>
     <ul>
       <li v-for="infos in info" v-bind:key="infos">{{infos}}</li>
     </ul>
@@ -19,6 +24,7 @@ export default {
   name: "Content",
   data() {
     return {
+      hover: true,
       eiffel_tower: eiffel_tower,
       my_width: "300",
       info: [
@@ -42,5 +48,22 @@ div img.eiffel {
 .content ul {
   padding-top: 30px;
   padding-left: 30px;
+}
+
+/* Vue Animation  */
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 1s;
+}
+
+.fade-leave {
+  opacity: 1;
+}
+
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
 }
 </style>
